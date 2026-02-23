2a340e692cfca8230d69786d86219fc3a1a6831b
balanta1 <sebastianbalanta79@gmail.com>
2026-02-23 17:17:41 +0000

    ok

--- clase4/Ejercicio42A.java
+++ clase4/Ejercicio42A.java
@@ -7,9 +7,11 @@ public class Ejercicio42A {
         System.out.println("Punto de ebullicion");
         int temperatura = sc.nextInt();
         //condicional
-        if (temperatura == 100);{
-             System.out.println("por debajo del punto de ebullicion del agua");
-        }else
+        if (temperatura == 100) {
+             System.out.println("por encima del punto de ebullicion del agua");
+        } else {
+            System.out.println("por debajo del punto de ebullicion del agua");
+        }
         sc.close();
     }
 }
