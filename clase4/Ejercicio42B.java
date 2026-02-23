be0a43513d60ffaa1bb561883d14cb73d3ff87d0
balanta1 <sebastianbalanta79@gmail.com>
2026-02-23 17:29:04 +0000

    el nombre del ejecicio

--- clase4/Ejercicio42B.java
+++ clase4/Ejercicio42B.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class Ejercicio42A {
+public class Ejercicio42B {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // pedir el punto de ebullicion del agua del usuario
         System.out.println("Punto de ebullicion");
         int temperatura = sc.nextInt();
-        //condicional
+        // condicional
         if (temperatura == 100) {
-             System.out.println("por encima del punto de ebullicion del agua");
+            System.out.println("por encima del punto de ebullicion del agua");
         } else {
             System.out.println("por debajo del punto de ebullicion del agua");
         }
