e49d52e478808058206206dc806b961f3745a762
balanta1 <sebastianbalanta79@gmail.com>
2026-03-02 14:51:42 +0000

    avance

---
+++ parcial/parcilafinal.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class parcilafinal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int CLAVE <=1234, SALDOINICIAL <=800000;
+        int retiro= Entero;
+        int claveUsuario;
+        int nuevoSaldo;
+        
+        System.out.println("claveUsuario");
+        if (CLAVE==1234){
+            System.out.println("Bienvenido automatico de la uniajc");
+            System.out.println("Digite la cantidad a digiatar");
+        }
+        System.out.println("retiro");
+        if (retiro>200000 && retiro == Entero + 2000)
+            if (retiro <= SALDOINICIAL && nuevoSaldo == SALDOINICIAL-retiro){
+            System.out.print("su saldo actual es:",nuevoSaldo,"Muchas gracias!!!!");
+        }else{
+            System.out.println("el valor solicitado mas el costo de la transaccion es mayor al saldo");
+        }else{
+            if (retiro <= SALDOINICIAL && nuevoSaldo = SALDOINICIAL-retiro){
+                System.out.println("el valor solicitado es mayor al saldo actual");
+        }else{
+            System.out.println("su clave no es correcta intenta nuevamente");
+            }
+        }
+    }
+
+}
