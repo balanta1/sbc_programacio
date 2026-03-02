4bbeb3ea12c14be6650d8b5d20e3e4e2a1403597
balanta1 <sebastianbalanta79@gmail.com>
2026-03-02 16:49:22 +0000

    parcial

--- parcial/parcilafinal.java
+++ parcial/parcilafinal.java
@@ -3,29 +3,55 @@ import java.util.Scanner;
 public class parcilafinal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int CLAVE <=1234, SALDOINICIAL <=800000;
-        int retiro= Entero;
-        int claveUsuario;
-        int nuevoSaldo;
-        
-        System.out.println("claveUsuario");
-        if (CLAVE==1234){
+        int CLAVE=1234;
+        int SALDOINICIAL =800000;
+        int claveUsuario=1234,retiro=0,nuevoSaldo;
+
+        System.out.println("Digite la clave de Usuario");
+        claveUsuario = sc.nextInt();
+
+        if (CLAVE == claveUsuario){
             System.out.println("Bienvenido automatico de la uniajc");
             System.out.println("Digite la cantidad a digiatar");
+            retiro = sc.nextInt();
         }
         System.out.println("retiro");
-        if (retiro>200000 && retiro == Entero + 2000)
-            if (retiro <= SALDOINICIAL && nuevoSaldo == SALDOINICIAL-retiro){
-            System.out.print("su saldo actual es:",nuevoSaldo,"Muchas gracias!!!!");
+        if (retiro>200000){
+            retiro = retiro + 2000;}
+
+            if (retiro <= SALDOINICIAL){
+                nuevoSaldo = SALDOINICIAL-retiro;
+            System.out.println("su saldo actual es:" +nuevoSaldo);
+             System.out.println("Muchas gracias!!!!");
         }else{
             System.out.println("el valor solicitado mas el costo de la transaccion es mayor al saldo");
-        }else{
-            if (retiro <= SALDOINICIAL && nuevoSaldo = SALDOINICIAL-retiro){
-                System.out.println("el valor solicitado es mayor al saldo actual");
-        }else{
-            System.out.println("su clave no es correcta intenta nuevamente");
-            }
+            retiro = sc.nextInt();
         }
+            if (retiro <= SALDOINICIAL){
+                nuevoSaldo = SALDOINICIAL-retiro;
+                System.out.println("su saldo actual es:" +nuevoSaldo+"Muchas gracias!!!!");
+            }else{
+                System.out.println("El valor solicitado es mayor el saldo actual");
+                retiro= sc.nextInt();
+                
+        }if(CLAVE == claveUsuario) {
+            System.out.println("su clave es correcta");
+            claveUsuario =sc.nextInt();
+        }else{
+            System.out.println("su clave es no correcta");
+            claveUsuario =sc.nextInt();
+        }  
+    sc.close();
     }
-
 }
+
+
+
+    
+
+
+
+
+
+
+    
